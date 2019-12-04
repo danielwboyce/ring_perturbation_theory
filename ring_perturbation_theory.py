ed7cd71a91e2908ab78ac9211a957937c014533e
danielwboyce <danielwboyce@gmail.com>
2019-12-04 13:23:58 -0700

    Cleaned up the code somewhat

--- ring_perturbation_theory.py
+++ ring_perturbation_theory.py
@@ -1,10 +1,10 @@
-# Calculating 2d ring-resonator modes, from the Meep tutorial.
 from __future__ import division
 
 import meep as mp
 import numpy as np
 from math import ceil
 from statistics import mean
+
 
 def main():
     n = 3.4                 # index of waveguide
@@ -20,20 +20,14 @@ def main():
     cell_size = mp.Vector3(sxy,sxy)
     resolution = 10
 
-    # Create a ring waveguide by two overlapping cylinders - later objects
-    # take precedence over earlier objects, so we put the outer cylinder first.
-    # and the inner (air) cylinder second.
-
     c1 = mp.Cylinder(radius=b, material=mp.Medium(index=n))
     c2 = mp.Cylinder(radius=a)
     geometry = [c1,c2]
 
-    # If we don't want to excite a specific mode symmetry, we can just
-    # put a single point source at some arbitrary place, pointing in some
-    # arbitrary direction.  We will only look for Ez-polarized modes.
+    # Exciting the first resonance mode (calculated with Harminiv)
 
-    fcen = 0.15             # pulse center frequency
-    df = 0.1                # pulse width (in frequency)
+    fcen = 0.118101372125519    # pulse center frequency
+    df = 0.1                    # pulse width (in frequency)
 
     sources = [mp.Source(mp.GaussianSource(fcen, fwidth=df), mp.Ez, mp.Vector3(r+0.1))]
 
@@ -65,23 +59,20 @@ def main():
         e_y_field = sim.get_field_point(mp.Ey, point)
         e_z_field = sim.get_field_point(mp.Ez, point)
         e_total_field = np.real(np.sqrt(e_x_field*np.conj(e_x_field) + e_y_field*np.conj(e_y_field) + e_z_field*np.conj(e_z_field)))
+        #e_total_field = np.real(np.sqrt(e_z_field * np.conj(e_z_field)))
         inner_ring_fields.append(e_total_field)
 
     for angle in angles_outer:
-        point = mp.Vector3(b * np.cos(angle), b * np.sin(angle), 0)
+        point = mp.Vector3(a * np.cos(angle), a * np.sin(angle), 0)
         e_x_field = sim.get_field_point(mp.Ex, point)
         e_y_field = sim.get_field_point(mp.Ey, point)
         e_z_field = sim.get_field_point(mp.Ez, point)
         e_total_field = np.real(np.sqrt(e_x_field*np.conj(e_x_field) + e_y_field*np.conj(e_y_field) + e_z_field*np.conj(e_z_field)))
+        #e_total_field = np.real(np.sqrt(e_z_field * np.conj(e_z_field)))
         outer_ring_fields.append(e_total_field)
 
     surface_integral = 2 * np.pi * b * (mean(inner_ring_fields) + mean(outer_ring_fields)) / 2
     print(f'\nThe value of the surface_integral is {surface_integral}')
-
-    # Output fields for one period at the end.  (If we output
-    # at a single time, we might accidentally catch the Ez field when it is
-    # almost zero and get a distorted view.)
-    # sim.run(mp.at_every(1/fcen/20, mp.output_efield_z), until=1/fcen)
 
 
 if __name__ == '__main__':
