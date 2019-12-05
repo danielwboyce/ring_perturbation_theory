206de71e88102310422b3aad0aaeabe509dfd42f
danielwboyce <danielwboyce@gmail.com>
2019-12-05 16:03:12 -0700

    First pass on finding relative error

--- ring_Ez_perturbation_theory.py
+++ ring_Ez_perturbation_theory.py
@@ -3,6 +3,7 @@ from __future__ import division
 import meep as mp
 import numpy as np
 from statistics import mean
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -80,13 +81,59 @@ def main():
         parallel_fields.append(e_total_field)
 
     numerator_surface_integral = 2 * np.pi * b * mean(parallel_fields)
-    print(f'\nThe value of numerator_surface_integral is {numerator_surface_integral}')
+    #print(f'\nThe value of numerator_surface_integral is {numerator_surface_integral}')
 
     denominator_surface_integral = sim.electric_energy_in_box(center=mp.Vector3(), size=mp.Vector3(2 * (b + pad/2)))
-    print(f'\nThe value of denominator_surface_integral is {denominator_surface_integral}')
+    #print(f'\nThe value of denominator_surface_integral is {denominator_surface_integral}')
 
     perturb_dw_dR = -resonance_0 * numerator_surface_integral / (4 * denominator_surface_integral)
-    print(f'\nThe value of perturb_dw_dR is {perturb_dw_dR}')
+    #print(f'\nThe value of perturb_dw_dR is {perturb_dw_dR}')
+
+    drs = np.logspace(start=-4, stop=-2, num=5)
+    print(f'My drs array is {drs}')
+    center_diff_dw_dR = []
+    for dr in drs:
+        sim.reset_meep()
+        w = 1 + dr  # width of waveguide
+        print(f'My current dr is {dr}')
+        b = a + w
+
+        fcen = resonance_0
+        df = 0.01
+
+        sources = [mp.Source(mp.GaussianSource(fcen, fwidth=df), mp.Ez, mp.Vector3(r + 0.1))]
+
+        geometry = [mp.Block(center=mp.Vector3(a + (w / 2)),
+                             size=mp.Vector3(w, 1e20, 1e20),
+                             material=mp.Medium(index=n))]
+
+        sim = mp.Simulation(cell_size=cell,
+                            geometry=geometry,
+                            boundary_layers=pml_layers,
+                            resolution=resolution,
+                            sources=sources,
+                            dimensions=dimensions,
+                            m=m)
+
+        h = mp.Harminv(mp.Ez, mp.Vector3(r + 0.1), fcen, df)
+        sim.run(mp.after_sources(h), until_after_sources=200)
+
+        resonance_dr = h.modes[0].freq
+        dw_dR = (resonance_dr - resonance_0) / dr
+        center_diff_dw_dR.append(dw_dR)
+
+    relative_errors = [(dw_dR - perturb_dw_dR) / perturb_dw_dR for dw_dR in center_diff_dw_dR]
+    if mp.am_master():
+        plt.figure(dpi=150)
+        plt.loglog(drs, relative_errors, 'bo-', label='relative error')
+        plt.grid(True, which='both', ls='-')
+        plt.xlabel('(perturbation amount $dr$)')
+        plt.ylabel('relative error between \ncenter-difference and perturbation theory')
+        plt.legend(loc='upper left')
+        plt.title('Comparison of Perturbation Theory and \nCenter-Difference Calculations in Finding $dw/dR$')
+        plt.tight_layout()
+        plt.show()
+        plt.savefig('ring_Ez_perturbation_theory.png')
 
 
 if __name__ == '__main__':
