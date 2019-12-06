4ab217c2d8b600cbb101d9fb1d4ea08f74b2754a
danielwboyce <danielwboyce@gmail.com>
2019-12-06 14:15:55 -0700

    Visualization does work in Cartesian coordinates.

--- ring_Hz_see_fields.py
+++ ring_Hz_see_fields.py
@@ -8,46 +8,43 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    n = 3.4                 # index of waveguide
-    r = 1
-    a = r                   # inner radius of ring
-    w = 1                   # width of waveguide
-    b = a + w               # outer radius of ring
-    pad = 4                 # padding between waveguide and edge of PML
+    n = 3.4  # index of waveguide
+    w = 1  # width of waveguide
+    r = 1  # inner radius of ring
+    pad = 4  # padding between waveguide and edge of PML
+    dpml = 2  # thickness of PML
+    sxy = 2 * (r + w + pad + dpml)  # cell size
 
-    dpml = 2                # thickness of PML
-    pml_layers = [mp.PML(dpml)]
+    # Create a ring waveguide by two overlapping cylinders - later objects
+    # take precedence over earlier objects, so we put the outer cylinder first.
+    # and the inner (air) cylinder second.
 
-    resolution = 10
+    c1 = mp.Cylinder(radius=r + w, material=mp.Medium(index=n))
+    c2 = mp.Cylinder(radius=r)
 
-    sr = b + pad + dpml            # radial size (cell is from 0 to sr)
-    dimensions = mp.CYLINDRICAL    # coordinate system is (r,phi,z) instead of (x,y,z)
-    cell = mp.Vector3(sr, 0, 0)
+    # If we don't want to excite a specific mode symmetry, we can just
+    # put a single point source at some arbitrary place, pointing in some
+    # arbitrary direction.  We will only look for Ez-polarized modes.
 
-    m = 5
+    fcen = 0.15  # pulse center frequency
+    df = 0.1  # pulse width (in frequency)
 
-    geometry = [mp.Block(center=mp.Vector3(a + (w / 2)),
-                         size=mp.Vector3(w, 1e20, 1e20),
-                         material=mp.Medium(index=n))]
+    src = mp.Source(mp.GaussianSource(fcen, fwidth=df), mp.Hz, mp.Vector3(r + 0.1))
+    #src = [mp.Source(mp.GaussianSource(fcen, fwidth=df), mp.Ex, mp.Vector3(r + 0.1), amplitude=1),
+    #       mp.Source(mp.GaussianSource(fcen, fwidth=df), mp.Ey, mp.Vector3(r + 0.1), amplitude=1j)]
 
-    # Finding a resonance mode with a high Q-value (calculated with Harminv)
-
-    fcen = 0.15         # pulse center frequency
-    df = 0.1            # pulse width (in frequency)
-
-    sources = [mp.Source(mp.GaussianSource(fcen, fwidth=df), mp.Hz, mp.Vector3(r+0.1), amplitude=1)]
-
-    sim = mp.Simulation(cell_size=cell,
-                        geometry=geometry,
-                        boundary_layers=pml_layers,
-                        resolution=resolution,
-                        sources=sources,
-                        dimensions=dimensions,
-                        m=m)
+    sim = mp.Simulation(cell_size=mp.Vector3(sxy, sxy),
+                        geometry=[c1, c2],
+                        sources=[src],
+                        resolution=10,
+                        symmetries=[mp.Mirror(mp.Y)],
+                        boundary_layers=[mp.PML(dpml)])
 
     sim.run(until_after_sources=200)
 
-    sim.plot2D(ax=None)
+    sim.plot2D(fields=mp.Hz,
+               field_parameters={'alpha':0.8, 'cmap':'RdBu', 'interpolation':'none'},
+               boundary_parameters={'hatch':'o', 'linewidth':1.5, 'facecolor':'y', 'edgecolor':'b', 'alpha':0.3})
     plt.show()
 
 
