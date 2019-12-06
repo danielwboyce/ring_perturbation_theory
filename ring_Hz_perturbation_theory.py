Fixed numerator_surface_integral calculation for the Hz case to include the adjustment for the perpendicular fields.

ring_Hz_perturbation_theory.py
@@ -92,7 +92,7 @@ def main():
         temp_parallel_field = np.real(np.sqrt(e_p_field * np.conj(e_p_field) + e_z_field * np.conj(e_z_field)))
         parallel_fields.append(temp_parallel_field)
 
-    numerator_surface_integral = 2 * np.pi * b * mean(parallel_fields)
+    numerator_surface_integral = 2 * np.pi * b * (mean(parallel_fields) - mean(perpendicular_fields))
     print(f'\nThe value of numerator_surface_integral is {numerator_surface_integral}')
 
     denominator_surface_integral = sim.electric_energy_in_box(center=mp.Vector3((b + pad/2) / 2), size=mp.Vector3(b + pad/2))